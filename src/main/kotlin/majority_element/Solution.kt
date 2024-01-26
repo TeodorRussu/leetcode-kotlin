bb0cccd436bb1cd3b97dd7534a4209e9166dedd8
teodorrussu <teodor_russu@yahoo.com>
2024-01-26 20:12:22 +0100

    majority element with Boyer-Moore Voting Algorithm

--- src/main/kotlin/majority_element/Solution.kt
+++ src/main/kotlin/majority_element/Solution.kt
@@ -1,21 +1,16 @@
 package majority_element
 
-import java.util.TreeMap
-
 class Solution {
     fun majorityElement(nums: IntArray): Int {
-        val elementsCounter = mutableMapOf<Int, Int>()
-        var majorityElement = nums[0]
-        var majorityElementCount = 0
-
+        var counter = 0
+        var candidate = 0
         nums.forEach {
-            val currentCount = elementsCounter.getOrDefault(key = it, defaultValue = 0) + 1
-            elementsCounter[it] = currentCount
-            if (currentCount > majorityElementCount) {
-                majorityElement = it
-                majorityElementCount = currentCount
+            if (counter == 0) {
+                candidate = it
             }
+            counter += if (it == candidate) 1 else -1
+
         }
-        return majorityElement
+        return candidate
     }
 }
