75a39caa279354011ae82019be11868f7dc111d4
teodorrussu <teodor_russu@yahoo.com>
2024-02-23 19:59:29 +0100

    optimize the algorithm

--- src/main/kotlin/rotate_array/Solution.kt
+++ src/main/kotlin/rotate_array/Solution.kt
@@ -2,18 +2,27 @@ package rotate_array
 
 class Solution {
     fun rotate(nums: IntArray, k: Int): Unit {
-
+        val step = k % nums.size
         val arrayLastIndex = nums.size - 1
-        for (i in 0..<k) {
-            var tempIndex = arrayLastIndex
 
-            while (tempIndex > 0) {
-                //swap
-                val tempNum = nums[tempIndex]
-                nums[tempIndex] = nums[tempIndex - 1]
-                nums[tempIndex - 1] = tempNum
-                tempIndex--
-            }
+        //reverse the entire array
+        reverseArray(nums, 0, arrayLastIndex)
+        //reverse the numbers with index between 0 .. step
+        reverseArray(nums, 0, step - 1)
+        //reverse the numbers with index between step .. array end
+        reverseArray(nums, step, nums.size - 1)
+    }
+
+    fun reverseArray(nums: IntArray, from: Int, to: Int) {
+        var start = from
+        var end = to
+
+        while (start < end) {
+            val temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start++
+            end--
         }
     }
 }
--- src/test/kotlin/rotate_array/SolutionTest.kt
+++ src/test/kotlin/rotate_array/SolutionTest.kt
@@ -30,6 +30,9 @@ class SolutionTest {
             return listOf(
                 Arguments.of(intArrayOf(1, 2, 3, 4, 5, 6, 7), 3, intArrayOf(5, 6, 7, 1, 2, 3, 4)),
                 Arguments.of(intArrayOf(-1, -100, 3, 99), 2, intArrayOf(3, 99, -1, -100)),
+                Arguments.of(intArrayOf(1), 0, intArrayOf(1)),
+                Arguments.of(intArrayOf(1), 2, intArrayOf(1)),
+                Arguments.of(intArrayOf(1,2), 3, intArrayOf(2,1)),
             )
         }
     }
